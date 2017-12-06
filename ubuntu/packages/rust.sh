33d014570585528597d31f17a070387bbba1794d
Josh Benham <joshbenham@gmail.com>
2017-12-06 11:04:33 +1100

    Starting add some packages

--- ubuntu/packages/rust.sh
+++ ubuntu/packages/rust.sh
@@ -5,3 +5,27 @@ ask_question " <- Do you want to install Rust? [y|n] "
 if said_yes ; then
     curl https://sh.rustup.rs -sSf | sh
 fi
+
+
+headline " -> Installing Rust Component Packages"
+
+packagelist=(
+
+rust-analysis
+rust-src
+
+)
+
+rustup component add ${packagelist[@]}
+
+
+headline " -> Installing Rust Cargo Packages"
+
+packagelist=(
+
+racer
+rustfmt
+
+)
+
+cargo install ${packagelist[@]}
