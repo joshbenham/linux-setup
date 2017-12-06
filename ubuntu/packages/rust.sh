f51ab11c6304be1450efa9aae4a01244914d5f5c
Josh Benham <joshbenham@gmail.com>
2017-12-06 11:14:03 +1100

    Updated the rust installers

--- ubuntu/packages/rust.sh
+++ ubuntu/packages/rust.sh
@@ -4,6 +4,9 @@ ask_question " <- Do you want to install Rust? [y|n] "
 
 if said_yes ; then
     curl https://sh.rustup.rs -sSf | sh
+    source ~/.bashrc
+    rustup install nightly
+    rustup default nightly
 fi
 
 
@@ -16,7 +19,7 @@ rust-src
 
 )
 
-rustup component add ${packagelist[@]}
+rustup component add ${packagelist[@]} --toolchain nightly
 
 
 headline " -> Installing Rust Cargo Packages"
@@ -25,6 +28,7 @@ packagelist=(
 
 racer
 rustfmt
+clippy
 
 )
 
