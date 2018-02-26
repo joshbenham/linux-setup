a601c28ee8e575b8c4d6d39cc10fc9d8894cf589
Josh Benham <joshbenham@gmail.com>
2018-02-26 21:56:35 +1100

    Added PHP7.2 as a dependency

--- ubuntu/packages/aptitude.sh
+++ ubuntu/packages/aptitude.sh
@@ -117,6 +117,21 @@ php7.0-zip
 php7.0
 libapache2-mod-php7.0
 
+php7.2-cgi
+php7.2-curl
+php7.2-gd
+php7.2-intl
+php7.2-json
+php7.2-mbstring
+php7.2-mysql
+php7.2-soap
+php7.2-sqlite3
+php7.2-xml
+php7.2-xmlrpc
+php7.2-zip
+php7.2
+libapache2-mod-php7.2
+
 php5.6-cgi
 php5.6-curl
 php5.6-gd
