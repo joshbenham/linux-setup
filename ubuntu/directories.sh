1fc15abb18b319238423e7611be9fa44d1da9e99
Josh Benham <joshbenham@gmail.com>
2018-05-06 23:06:02 +1000

    Cleaned up how the directories installer works

--- ubuntu/directories.sh
+++ ubuntu/directories.sh
@@ -1,60 +1,29 @@
 #!/bin/bash
 
-ask_question " <- Would you like to setup the Code folder [y|n] "
-echo
+NAME=`whoami` 
+FOLDERS=(
+".password-store"
+".thunderbird"
 
-if said_yes ; then
-    NAME=`whoami`
-    COMMAND="ln -sf /media/$NAME/Data/Code /home/$NAME/Code"
-    $COMMAND
-fi
+"Applications"
+"Backups"
+"Code"
+"Desktop"
+"Documents"
+"Downloads"
+"Games"
+"Music"
+"Pictures"
+"Public"
+"Templates"
+"Videos"
+)
 
+for FOLDER in "${FOLDERS[@]}"; do
+    ask_question " <- Would you like to setup the $FOLDER folder [y|n]"
+    if said_yes; then
+        COMMAND="ln -sf /media/$NAME/Data/$FOLDER /home/$NAME/$FOLDER"
+	$COMMAND
+    fi
+done
 
-ask_question " <- Would you like to setup the Applications folder [y|n] "
-echo
-
-if said_yes ; then
-    NAME=`whoami`
-    COMMAND="ln -sf /media/$NAME/Data/Applications /home/$NAME/Applications"
-    $COMMAND
-fi
-
-
-ask_question " <- Would you like to setup the Documents folder [y|n] "
-echo
-
-if said_yes ; then
-    NAME=`whoami`
-    COMMAND="ln -sf /media/$NAME/Data/Documents /home/$NAME/Documents"
-    $COMMAND
-fi
-
-
-ask_question " <- Would you like to setup the Backups folder [y|n] "
-echo
-
-if said_yes ; then
-    NAME=`whoami`
-    COMMAND="ln -sf /media/$NAME/Data/Backups /home/$NAME/Backups"
-    $COMMAND
-fi
-
-
-ask_question " <- Would you like to setup the Password Store folder [y|n] "
-echo
-
-if said_yes ; then
-    NAME=`whoami`
-    COMMAND="ln -sf /media/$NAME/Data/.password-store /home/$NAME/.password-store"
-    $COMMAND
-fi
-
-
-ask_question " <- Would you like to setup the Thunderbird folder [y|n] "
-echo
-
-if said_yes ; then
-    NAME=`whoami`
-    COMMAND="ln -sf /media/$NAME/Data/.thunderbird /home/$NAME/.thunderbird"
-    $COMMAND
-fi
