d07b4c80402fbd43fa9514b17a345d7ad841009e
Josh Benham <joshbenham@gmail.com>
2018-05-07 15:58:48 +1000

    Added VSCode as a dependency

--- ubuntu/packages/other.sh
+++ ubuntu/packages/other.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+FILENAME="/usr/bin/code"
+if [[ ! -f "$FILENAME" ]] ; then
+    headline " -> Installing VS Code"
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    sudo apt-get update
+    sudo apt-get install code
+    echo
+fi
+
+
 FILENAME="/usr/bin/subl"
 if [[ ! -f "$FILENAME" ]] ; then
     headline " -> Installing Sublime Text (Stable)"
