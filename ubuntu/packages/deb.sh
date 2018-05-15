dc85f61ec144b4dda95ffaf116918b1b016ee683
Josh Benham <joshbenham@gmail.com>
2018-05-15 16:27:53 +1000

    Updated teamviewer link

--- ubuntu/packages/deb.sh
+++ ubuntu/packages/deb.sh
@@ -5,7 +5,7 @@ declare -A debs
 debs=(
 
 [/usr/bin/google-chrome]=https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-[/usr/bin/teamviewer]=http://download.teamviewer.com/download/teamviewer_i386.deb
+[/usr/bin/teamviewer]=https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 [/usr/bin/screamingfrogseospider]=https://www.screamingfrog.co.uk/products/seo-spider/screamingfrogseospider_6.0_all.deb
 
 )
