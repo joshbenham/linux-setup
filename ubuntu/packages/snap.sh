5517cb5e463b5820c85c66b44bbb6a9e1b5a1b29
Josh Benham <joshbenham@gmail.com>
2018-11-07 16:58:49 +1100

    Added skype as a snap

--- ubuntu/packages/snap.sh
+++ ubuntu/packages/snap.sh
@@ -16,5 +16,6 @@ sudo snap install ${packages[@]}
 sudo snap install sublime-text --classic
 sudo snap install vscode --classic
 sudo snap install slack --classic
+sudo snap install skype --classic
 
 sudo snap refresh
