6d318b941e7b4699be6f54a93450a9064366b9e4
Josh Benham <joshbenham@gmail.com>
2020-01-05 20:48:32 +1100

    Updated to PHP7.4 from PHP7.3

--- ubuntu/packages/aptitude.sh
+++ ubuntu/packages/aptitude.sh
@@ -100,21 +100,21 @@ php7.0-zip
 php7.0
 libapache2-mod-php7.0
 
-php7.3-bcmath
-php7.3-cgi
-php7.3-curl
-php7.3-gd
-php7.3-intl
-php7.3-json
-php7.3-mbstring
-php7.3-mysql
-php7.3-soap
-php7.3-sqlite3
-php7.3-xml
-php7.3-xmlrpc
-php7.3-zip
-php7.3
-libapache2-mod-php7.3
+php7.4-bcmath
+php7.4-cgi
+php7.4-curl
+php7.4-gd
+php7.4-intl
+php7.4-json
+php7.4-mbstring
+php7.4-mysql
+php7.4-soap
+php7.4-sqlite3
+php7.4-xml
+php7.4-xmlrpc
+php7.4-zip
+php7.4
+libapache2-mod-php7.4
 
 phpmyadmin
 mysql-workbench
