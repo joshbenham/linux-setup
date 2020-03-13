636cbe81a20291faa28a1f16d60714882e2005f9
Josh Benham <joshbenham@gmail.com>
2020-03-13 16:08:14 +1100

    Added php7.3 as a requirement

--- ubuntu/packages/aptitude.sh
+++ ubuntu/packages/aptitude.sh
@@ -99,6 +99,22 @@ php7.0-zip
 php7.0
 libapache2-mod-php7.0
 
+php7.3-bcmath
+php7.3-cgi
+php7.3-curl
+php7.3-gd
+php7.3-intl
+php7.3-json
+php7.3-mbstring
+php7.3-mysql
+php7.3-soap
+php7.3-sqlite3
+php7.3-xml
+php7.3-xmlrpc
+php7.3-zip
+php7.3
+libapache2-mod-php7.3
+
 php7.4-bcmath
 php7.4-cgi
 php7.4-curl
