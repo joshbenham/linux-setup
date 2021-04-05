2bd9752e9ee38289d9139a1f510c3b393745dbb2
Josh Benham <joshbenham@gmail.com>
2021-04-05 21:32:01 +1000

    Changed linux-setup to be in home directory

--- ubuntu/packages/other.sh
+++ ubuntu/packages/other.sh
@@ -13,18 +13,18 @@ fi
 headline " -> Setting up Sublime Text 3 Themes"
 
 DIRECTORY="$HOME/.config/sublime-text-3/Packages/User/"
-ln -sf "$HOME/Code/Personal/linux-setup/data/sublime-text-3/Package Control.sublime-settings" "$DIRECTORY/Package Control.sublime-settings"
-ln -sf "$HOME/Code/Personal/linux-setup/data/sublime-text-3/Preferences.sublime-settings" "$DIRECTORY/Preferences.sublime-settings"
-ln -sf "$HOME/Code/Personal/linux-setup/data/sublime-text-3/Default (Linux).sublime-keymap" "$DIRECTORY/Default (Linux).sublime-keymap"
+ln -sf "$HOME/linux-setup/data/sublime-text-3/Package Control.sublime-settings" "$DIRECTORY/Package Control.sublime-settings"
+ln -sf "$HOME/linux-setup/data/sublime-text-3/Preferences.sublime-settings" "$DIRECTORY/Preferences.sublime-settings"
+ln -sf "$HOME/linux-setup/data/sublime-text-3/Default (Linux).sublime-keymap" "$DIRECTORY/Default (Linux).sublime-keymap"
 
 
 headline " -> Setting up Konsole Themes"
 
 DIRECTORY="$HOME/.local/share/konsole"
-SRCDIRECTORY="$HOME/Code/Personal/linux-setup/data/konsole"
+SRCDIRECTORY="$HOME/linux-setup/data/konsole"
 for FILENAME in $SRCDIRECTORY/*.colorscheme; do
     NEWFILENAME=$(basename "$FILENAME")
-    cp -f "$HOME/Code/Personal/linux-setup/data/konsole/$NEWFILENAME" "$DIRECTORY/$NEWFILENAME"
+    cp -f "$HOME/linux-setup/data/konsole/$NEWFILENAME" "$DIRECTORY/$NEWFILENAME"
 done
 
 
