a05c3eec383bea1289674666dbe67b2e6c2e91d6
Josh Benham <joshbenham@gmail.com>
2022-06-03 10:25:46 +1000

    Added PHP8.1

--- ubuntu/packages/aptitude.sh
+++ ubuntu/packages/aptitude.sh
@@ -122,6 +122,20 @@ php8.0-zip
 php8.0
 libapache2-mod-php8.0
 
+php8.1-cgi
+php8.1-curl
+php8.1-gd
+php8.1-intl
+php8.1-mbstring
+php8.1-mysql
+php8.1-soap
+php8.1-sqlite3
+php8.1-xml
+php8.1-xmlrpc
+php8.1-zip
+php8.1
+libapache2-mod-php8.1
+
 phpmyadmin
 
 )
