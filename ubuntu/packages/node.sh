b2e7cce1799ff0faf790d915e2fcdf0b8c041e59
Josh Benham <joshbenham@gmail.com>
2022-12-14 17:39:11 +1100

    Updated to use Node 16

--- ubuntu/packages/node.sh
+++ ubuntu/packages/node.sh
@@ -3,7 +3,7 @@
 ask_question " <- Do you want to install/update Node? [y|n] "
 
 if said_yes ; then
-    curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
+    curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
     sudo apt-get install -y nodejs
     source ~/.bashrc
 fi
